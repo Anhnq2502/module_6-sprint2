2a2dca003d41bb234a144275312aafeefaa146a8
Ddloyd <quocanh25022001@gmail.com>
2023-06-21 15:31:28 +0700

    AnhNQ update jwt spring boot

--- be/be/src/main/java/com/example/be/controller/AuthController.java
+++ be/be/src/main/java/com/example/be/controller/AuthController.java
@@ -24,6 +24,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.HashSet;
 import java.util.Set;
+
 @CrossOrigin("*")
 @RestController
 @RequestMapping("/api/auth")
@@ -38,30 +39,27 @@ public class AuthController {
     AuthenticationManager authenticationManager;
     @Autowired
     JwtProvider jwtProvider;
+
     @PostMapping("/signup")
-    public ResponseEntity<?> register(@Valid @RequestBody SignUpForm signUpForm){
-        if(userService.existsByUsername(signUpForm.getUsername())){
+    public ResponseEntity<?> register(@Valid @RequestBody SignUpForm signUpForm) {
+        if (userService.existsByUsername(signUpForm.getUsername())) {
             return new ResponseEntity<>(new ResponseMessage("nouser"), HttpStatus.OK);
         }
-        if(userService.existsByEmail(signUpForm.getEmail())){
+        if (userService.existsByEmail(signUpForm.getEmail())) {
             return new ResponseEntity<>(new ResponseMessage("noemail"), HttpStatus.OK);
         }
         User user = new User(signUpForm.getName(), signUpForm.getUsername(), signUpForm.getEmail(), passwordEncoder.encode(signUpForm.getPassword()));
         Set<String> strRoles = signUpForm.getRoles();
         Set<Role> roles = new HashSet<>();
-        strRoles.forEach(role ->{
-            switch (role){
+        strRoles.forEach(role -> {
+            switch (role) {
                 case "admin":
-                    Role adminRole = roleService.findByName(RoleName.ADMIN).orElseThrow(()->
+                    Role adminRole = roleService.findByName(RoleName.ADMIN).orElseThrow(() ->
                             new RuntimeException("Role NOT FOUND"));
                     roles.add(adminRole);
                     break;
-                case "pm":
-                    Role pmRole = roleService.findByName(RoleName.PM).orElseThrow(
-                            ()-> new RuntimeException("Role NOT FOUND"));
-                    roles.add(pmRole);
-                    break;
-                default: Role userRole = roleService.findByName(RoleName.USER).orElseThrow(()->new RuntimeException("ROLE NOT FOUND"));
+                default:
+                    Role userRole = roleService.findByName(RoleName.USER).orElseThrow(() -> new RuntimeException("ROLE NOT FOUND"));
                     roles.add(userRole);
             }
         });
@@ -69,6 +67,7 @@ public class AuthController {
         userService.save(user);
         return new ResponseEntity<>(new ResponseMessage("create success!!!"), HttpStatus.OK);
     }
+
     @PostMapping("/signin")
     public ResponseEntity<?> login(@Valid @RequestBody SignInForm signInForm) {
         Authentication authentication = authenticationManager.authenticate(
@@ -76,6 +75,7 @@ public class AuthController {
         SecurityContextHolder.getContext().setAuthentication(authentication);
         String token = jwtProvider.createToken(authentication);
         UserPrinciple userPrinciple = (UserPrinciple) authentication.getPrincipal();
-        return ResponseEntity.ok(new JwtResponse(token,userPrinciple.getName(), userPrinciple.getAuthorities()));
+        return ResponseEntity.ok(new JwtResponse(token, userPrinciple.getName(), userPrinciple.getAuthorities()));
+
     }
 }
--- be/be/src/main/java/com/example/be/model/RoleName.java
+++ be/be/src/main/java/com/example/be/model/RoleName.java
@@ -2,5 +2,5 @@ package com.example.be.model;
 
 public enum RoleName {
     USER,
-    PM, ADMIN
+    ADMIN
 }
